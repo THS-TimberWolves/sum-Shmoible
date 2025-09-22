ee6e01f98f7f6d46b7aea2d5e3a1ded3ecf37604
Otani <164045655+Shmoible@users.noreply.github.com>
2025-09-22 11:25:10 -0600

    Update while_summation.py

--- while_summation.py
+++ while_summation.py
@@ -1,1 +1,7 @@
-#while summation code following directions from Readme
+user_num = int(input())
+times_looped = 0
+total = 0
+while times_looped <= user_num:
+  total += times_looped
+  times_looped = times_looped + 1
+print (total)
